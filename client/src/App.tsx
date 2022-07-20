dbc1c5154b45e00bb5ccac71052f1b4e5ca5a106
dzoniD <edvard_dantes@hotmail.com>
2022-07-20 23:59:13 +0200

    added AddTodo and get Todo on client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,14 @@
 import { Box } from "@mantine/core";
 import useSWR from "swr";
+import AddTodo from "./components/addTodo";
 import "./App.css";
+
+export interface Todo {
+  id: number;
+  title: string;
+  body: string;
+  done: boolean;
+}
 
 export const ENDPOINT = "http://localhost:4000";
 
@@ -8,9 +16,14 @@ const fetcher = (url: string) =>
   fetch(`${ENDPOINT}/${url}`).then((r) => r.json());
 
 function App() {
-  const { data, mutate } = useSWR("api/todos", fetcher);
+  const { data, mutate } = useSWR<Todo[]>("api/todos", fetcher);
 
-  return <Box>{JSON.stringify(data)}</Box>;
+  return (
+    <Box>
+      {JSON.stringify(data)}
+      <AddTodo mutate={mutate} />
+    </Box>
+  );
 }
 
 export default App;
